f8fff48b564d202566fb727ba95afc0afb18247f
MattKang <mattkang@users.noreply.github.com>
2020-10-08 19:25:41 -0400

    Assert data paths in tests

--- tests/tests_main.cpp
+++ tests/tests_main.cpp
@@ -9,12 +9,13 @@
 #include "catch.hpp"
 #include <filesystem>
 
-const auto projectRoot = std::filesystem::current_path().parent_path().parent_path().string();
+const auto projectRoot = std::filesystem::current_path().parent_path().parent_path();
 
 TEST_CASE("Read tuples with ignored columns")
 {
-    const auto dataPath = projectRoot + "/data/test.csv";
-    auto tups = csv::toTuples<int, float, bool, csv::ignore, csv::ignore, int>(dataPath);
+    const auto dataPath = projectRoot / "data/test.csv";
+    assert(std::filesystem::exists(dataPath));
+    auto tups = csv::toTuples<int, float, bool, csv::ignore, csv::ignore, int>(dataPath.string());
     const auto& t = tups.front();
     CHECK(std::get<0>(t) == 1);
     CHECK(std::get<2>(t) == true);
@@ -23,15 +24,19 @@ TEST_CASE("Read tuples with ignored columns")
 
 TEST_CASE("Read header")
 {
-    const auto dataPath = projectRoot + "/data/test_int_with_header.csv";
-    auto header = csv::getHeader(dataPath);
+    const auto dataPath = projectRoot / "data/test_int_with_header.csv";
+    assert(std::filesystem::exists(dataPath));
+
+    auto header = csv::getHeader(dataPath.string());
     CHECK(header == std::vector<std::string>{"Index", "Age", "Score"});
 }
 
 TEST_CASE("Read arrays")
 {
-    const auto dataPath = projectRoot + "/data/test_float.csv";
-    auto arr = csv::toArrays<double, 4>(dataPath);
+    const auto dataPath = projectRoot / "data/test_float.csv";
+    assert(std::filesystem::exists(dataPath));
+
+    auto arr = csv::toArrays<double, 4>(dataPath.string());
     CHECK(arr == std::vector<std::array<double, 4>>{
             {3.3l, 25.0, 1.4738, 4789.1},
             {4.18374, 0.48734, 47839247.471890234, 0},
@@ -41,19 +46,23 @@ TEST_CASE("Read arrays")
 
 TEST_CASE("Read arrays with header")
 {
-    const auto dataPath = projectRoot + "/data/test_int_with_header.csv";
+    const auto dataPath = projectRoot / "data/test_int_with_header.csv";
+    assert(std::filesystem::exists(dataPath));
+
     using Header = std::array<std::string, 3>;
     Header header;
-    auto arr = csv::toArrays<int, 3>(dataPath, header);
+    auto arr = csv::toArrays<int, 3>(dataPath.string(), header);
     CHECK(header == Header{"Index", "Age", "Score"});
 }
 
 TEST_CASE("Read vectors")
 {
-    const auto dataPath = projectRoot + "/data/test_int_with_header.csv";
+    const auto dataPath = projectRoot / "data/test_int_with_header.csv";
+    assert(std::filesystem::exists(dataPath));
+
     using Header = std::vector<std::string>;
     Header header;
-    auto arr = csv::toVectors<int>(dataPath, header);
+    auto arr = csv::toVectors<int>(dataPath.string(), header);
     CHECK(header == Header{"Index", "Age", "Score"});
     CHECK(arr == std::vector<std::vector<int>>{{1, 25, 100}, {2, 38, 87}, {3, 19, 55}});
 }
@@ -66,8 +75,10 @@ TEST_CASE("Array check")
 
 TEST_CASE("Tab delimiters")
 {
-    const auto dataPath = projectRoot + "/data/test.tsv";
-    auto tups = csv::toTuples<int, float, bool, int, int, int>(dataPath, '\t');
+    const auto dataPath = projectRoot / "data/test.tsv";
+    assert(std::filesystem::exists(dataPath));
+
+    auto tups = csv::toTuples<int, float, bool, int, int, int>(dataPath.string(), '\t');
     const auto& t = tups.back();
     CHECK(std::get<0>(t) == 2);
     CHECK(std::get<1>(t) == 5.04f);
@@ -79,8 +90,10 @@ TEST_CASE("Tab delimiters")
 
 TEST_CASE("Tab delimiter deduction")
 {
-    const auto dataPath = projectRoot + "/data/test.tsv";
-    auto tups = csv::toTuples<int, float, bool, int, int, int>(dataPath);
+    const auto dataPath = projectRoot / "data/test.tsv";
+    assert(std::filesystem::exists(dataPath));
+
+    auto tups = csv::toTuples<int, float, bool, int, int, int>(dataPath.string());
     const auto& t = tups.at(1);
     CHECK(std::get<0>(t) == 3);
     CHECK(std::get<1>(t) == 1.14f);
